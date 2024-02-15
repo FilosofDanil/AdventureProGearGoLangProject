75f894320dc49b8c4c09011ada06b5ebf0d67a74
FilosofDanil <serhio3347@gmail.com>
2024-02-15 18:00:46 +0100

    Framework Integration: Hello world extended

--- main.go
+++ main.go
@@ -5,18 +5,23 @@ import (
 	"github.com/labstack/echo/v4"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("", HelloWorld)
+	go provideLink()
 	if err := e.Start(":8080"); err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Link: http://localhost:8080")
 }
 
 func HelloWorld(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, "Hello World!")
+}
+
+func provideLink() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("Link: http://localhost:8080")
 }

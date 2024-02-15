3c8f47a4b5e033439d0ae500442a4db9f3fd1ec7
FilosofDanil <serhio3347@gmail.com>
2024-02-15 17:53:16 +0100

    Framework Integration: Imported echo/v4 high performance, light Go web framework and developed Hello World application.

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
+)
 
+func main() {
+	e := echo.New()
+	e.GET("", HelloWorld)
+	if err := e.Start(":8080"); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Link: http://localhost:8080")
+}
+
+func HelloWorld(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, "Hello World!")
 }
